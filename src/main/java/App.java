afe28f46d4a6208fcfcee3c62ca139d070185e04
Oleksii Mospan <alexey.mospan@gmail.com>
2022-04-25 02:06:58 +0300

    Ints too big. Need smaller

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 50;
+        int a = 1;
+        int b = 5;
         System.out.println(sum(a, b));
         System.out.println(dev(a, b));
     }
