945ba89c8f77456b5992fc1250397b609c99d65b
Oleksii-Mospan <89127223+Oleksii-Mospan@users.noreply.github.com>
2022-04-25 01:50:24 +0300

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) {
         int a = 10;
-        int b = 5;
+        int b = 50;
         System.out.println(sum(a, b));
         System.out.println(dev(a, b));
     }
