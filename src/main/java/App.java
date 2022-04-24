d849d42f336aa98613252e9e1aa349eaca0ba699
Oleksii Mospan <alexey.mospan@gmail.com>
2022-04-24 13:09:47 +0300

    New App.java added

---
+++ src/main/java/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println(sumAandB(a, b));
+        System.out.println(devAandB(a, b));
+    }
+
+    public static int sumAandB (int a, int b) {
+        return a + b;
+    }
+
+    public static int devAandB (int a, int b) {
+        return a / b;
+    }
+
+}
