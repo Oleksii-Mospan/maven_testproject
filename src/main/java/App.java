3555731a86116b094265a11a70a531ba51cfa287
Oleksii Mospan <alexey.mospan@gmail.com>
2022-04-24 13:19:37 +0300

    changed names: sumAandB() to sum() devAandB() to dev()

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,15 +2,15 @@ public class App {
     public static void main(String[] args) {
         int a = 10;
         int b = 5;
-        System.out.println(sumAandB(a, b));
-        System.out.println(devAandB(a, b));
+        System.out.println(sum(a, b));
+        System.out.println(dev(a, b));
     }
 
-    public static int sumAandB (int a, int b) {
+    public static int sum (int a, int b) {
         return a + b;
     }
 
-    public static int devAandB (int a, int b) {
+    public static int dev (int a, int b) {
         return a / b;
     }
 
